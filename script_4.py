536111e466b98b8e3ee2a4a80dfa1c8775ad5248
The Scientist <166526353+back2stars@users.noreply.github.com>
2025-01-07 08:09:03 +0000

    idk anymore

--- script_4.py
+++ script_4.py
@@ -2,13 +2,13 @@ from hijridate import *
 from datetime import *
 
 
-with open('output_3.js', 'w') as file:
-    file.write('const calendar = {\n')
+with open('output_4.js', 'w') as file:
+    file.write("const calendar = {'1924-08-01':'1343-01-01'")
     date = datetime(1924, 8, 1)
     while True:
         try:
             temp = Gregorian(date.year, date.month, date.day)
-            file.write(f'    "{temp.isoformat()}" : "{temp.to_hijri().isoformat()}",\n')
+            file.write(f",'{temp.isoformat()}':'{temp.to_hijri().isoformat()}'")
             date = date + timedelta(days=1)
         except:
             file.write('}')
