796252cc0aa815f167ebeaac6c1a3b3b627ef663
The Scientist <166526353+back2stars@users.noreply.github.com>
2025-01-10 07:46:34 +0000

    friday

--- calendar.js_generator.py
+++ calendar.js_generator.py
@@ -4,12 +4,12 @@ from datetime import *
 
 with open('calendar.js', 'w') as file:
     file.write('const CALENDAR = {\n')
-    date = datetime(1924, 8, 1)
+    current = datetime(1924, 8, 1)
     while True:
         try:
-            temp = Gregorian(date.year, date.month, date.day)
+            temp = Gregorian(current.year, current.month, current.day)
             file.write(f'    "{temp.isoformat()}" : "{temp.to_hijri().isoformat()}",\n')
-            date = date + timedelta(days=1)
+            current = current + timedelta(days=1)
         except:
             file.write('}')
             break
